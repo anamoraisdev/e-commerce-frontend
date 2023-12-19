abef42586bd5ed942d18b8d4bc44c0b64b91c14d
ana <anamorais.dev@gmail.com>
2023-12-19 15:45:50 -0300

    fixing navbar, menu and notice

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -2,19 +2,14 @@ import { BiSearch } from "react-icons/bi";
 
 const Menu = () => {
     const categories = [{ name: "adulto", id: 1 }, { name: "Infantil", id: 2 }, { name: "promocional", id: 3 },
-    { name: "lançamento", id: 4 }, { name: "lançamento", id: 5 }, { name: "lançamento", id: 6 },
-    { name: "lançamento", id: 7 }]
+    { name: "lançamento", id: 4 }, { name: "lançamento", id: 5 }, { name: "lançamento", id: 6 }]
 
     return (
-        <main className="w-screen p-2 flex flex-col gap-2 items-center xl:flex-row 2xl:flex-row">
-            <section className="flex items-center gap-2 justify-center">
-                <input placeholder="nome do produto" className="rounded-md w-[80%] px-6 text-sm border border-primary"></input>
-                <button><BiSearch color="gray" size={20} /></button>
-            </section>
+        <main className="w-full bg-secondary px-2 py-3">
 
             <section className="flex justify-center flex-wrap gap-2">
                 {categories && categories.map((category) =>
-                    <button key={category.id} className="text-primary border font-xs px-1 rounded-md hover:bg-complement hover:text-white">{category.name}</button>
+                    <button key={category.id} className="text-white border font-xs px-1 rounded-md hover:bg-complement hover:text-secondary">{category.name}</button>
 
                 )}
             </section >
--- app/components/notice.tsx
+++ app/components/notice.tsx
@@ -1,6 +1,6 @@
 const Notice = () => {
     return(
-        <main className="w-screen bg-complement flex justify-center p-1">
+        <main className="w-full bg-complement flex justify-center p-1">
             <p className="text-xs font-bold text-white">FRETE GRATIS PARA TODO PAIS</p>
         </main>
     )
